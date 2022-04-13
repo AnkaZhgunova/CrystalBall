5efda494b3300ab8a814c6e06f2d359bc57765b5
Aleksey <Tatarintsev>
2022-04-13 20:56:44 +0300

    initial commit

---
+++ lib/main_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/image.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: Image.asset(
+                  'assets/Vector.png',
+                ),
+              ),
+            ),
+            Spacer(),
+            Text(
+              'Задай вопрос',
+              style: TextStyle(
+                color: Color.fromRGBO(
+                  223,
+                  237,
+                  255,
+                  1,
+                ),
+                fontSize: 25,
+                fontWeight: FontWeight.w600,
+                fontFamily: 'Nunito',
+              ),
+            ),
+            Spacer(
+              flex: 4,
+            ),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Image.asset(
+                  'assets/image 1.png',
+                  height: 260,
+                  width: 260,
+                ),
+                Image.asset(
+                  'assets/iside_circle.png',
+                  height: 130,
+                  width: 130,
+                ),
+                TextButton(
+                  child: Text(
+                    'НАЧАТЬ',
+                    style: TextStyle(
+                      color: Color(0xFFDFEDFF),
+                      fontSize: 25,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: 'Nunito',
+                    ),
+                  ),
+                  onPressed: () {},
+                )
+              ],
+            ),
+            Spacer(
+              flex: 3,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
